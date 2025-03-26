784b3d7c1e41c37f2cc60ccbdb04f013ef890a77
EngeteloSia <164407779+EngeteloSia@users.noreply.github.com>
2025-03-26 17:38:08 +0200

    Completed The user class and the follow ups

---
+++ src/main/java/ac/za/mycput/factory/UserFactory.java
@@ -0,0 +1,35 @@
+package ac.za.mycput.factory;
+
+import ac.za.mycput.domain.User;
+import ac.za.mycput.until.Helper;
+import java.time.LocalDate;
+
+/*Engetelo Assistance Mathebane
+230227767
+ */
+public class UserFactory {
+
+    public static User createUser(String userId, String userName,String userSurname, String userDateOfBirth
+            ,String userEmail, String userAddress) {
+
+        if(Helper.isNullOrEmpty(userId) || Helper.isNullOrEmpty(userName) || Helper.isNullOrEmpty(userSurname) || Helper.isNullOrEmpty(userDateOfBirth) ||
+                Helper.isNullOrEmpty(userAddress) || Helper.isNullOrEmpty(userEmail))
+            return null;
+
+        if(Helper.isValidEmail(userEmail))
+            return null;
+
+        LocalDate dateOfBirth = Helper.getDateOfBirth(userId);
+
+        return new User.Builder()
+                .setUserId(Integer.parseInt(userId))
+                .setUserName(userName)
+                .setUserSurname(userSurname)
+                .setUserAddress(userAddress)
+                .setUserDateOfBirth(LocalDate.parse(userDateOfBirth))
+                .setUserEmail(userEmail)
+                .build();
+
+    }
+
+}
